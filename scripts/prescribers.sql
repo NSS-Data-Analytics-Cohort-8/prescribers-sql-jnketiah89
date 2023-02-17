380c5aee4af52e3fa355c78c1e8223de8c22cdb5
Jasmine Nketiah <jasmine.nketiah@gmail.com>
2023-02-16 21:30:45 -0600

    Q3b

--- scripts/prescribers.sql
+++ scripts/prescribers.sql
@@ -25,6 +25,7 @@ GROUP BY P.NPI,p.nppes_provider_last_org_name,p.nppes_provider_first_name,p.spec
 ORDER BY MAX(PR.total_claim_count) DESC;
 
 --2a. Which specialty had the most total number of claims (totaled over all drugs)?
+
 SELECT p.specialty_description,MAX(pr.total_claim_count) AS totalclaimcount
 FROM prescriber AS P
 INNER JOIN prescription AS PR
@@ -32,8 +33,10 @@ ON P.npi=PR.NPI
 GROUP BY p.specialty_description
 ORDER BY MAX(pr.total_claim_count) desc;
 
+--Answer: Family Practice
 
 --b. Which specialty had the most total number of claims for opioids?
+
 SELECT p.specialty_description,MAX(pr.total_claim_count) AS totalclaimcount
 FROM prescriber AS P
 INNER JOIN prescription AS PR
@@ -48,13 +51,40 @@ Select *
 FROM drug d
 where d.opioid_drug_flag = 'Y'
 
-c. Challenge Question: Are there any specialties that appear in the prescriber table that have no associated prescriptions in the prescription table?
+--c. Challenge Question: Are there any specialties that appear in the prescriber table that have no associated prescriptions in the prescription table?
 
-d. Difficult Bonus: Do not attempt until you have solved all other problems! For each specialty, report the percentage of total claims by that specialty which are for opioids. Which specialties have a high percentage of opioids?
+SELECT specialty_description,p.npi AS PRESCRIBEnpi,PR.npi AS PRESCRIPTIONNPI
+FROM prescriber AS P
+LEFT JOIN prescription AS PR
+ON P.npi=PR.npi
+WHERE PR.npi IS NULL;
 
-a. Which drug (generic_name) had the highest total drug cost?
+--Answer:Yes
 
-b. Which drug (generic_name) has the hightest total cost per day? Bonus: Round your cost per day column to 2 decimal places. Google ROUND to see how this works.
+
+--d. Difficult Bonus: Do not attempt until you have solved all other problems! For each specialty, report the percentage of total claims by that specialty which are for opioids. Which specialties have a high percentage of opioids?
+
+--a. Which drug (generic_name) had the highest total drug cost?
+SELECT DR.generic_name , MAX(PR.total_drug_cost) AS totatldrugcost
+FROM prescription AS PR
+LEFT JOIN drug AS DR
+ON PR.drug_name=DR.drug_name
+GROUP BY DR.generic_name
+ORDER BY MAX(PR.total_drug_cost) DESC;
+
+--Answer:Pirfenidone
+
+
+--b. Which drug (generic_name) has the hightest total cost per day? Bonus: Round your cost per day column to 2 decimal places. Google ROUND to see how this works.
+SELECT DR.generic_name , SUM(PR.total_drug_cost)/SUM(PR.total_30_day_fill_count) AS totalcostperday
+FROM prescription AS PR
+LEFT JOIN drug AS DR
+ON PR.drug_name=DR.drug_name
+GROUP BY DR.generic_name
+ORDER BY SUM(PR.total_drug_cost)/SUM(PR.total_30_day_fill_count) DESC;
+
+--Answer:Chenodiol
+
 
 a. For each drug in the drug table, return the drug name and then a column named 'drug_type' which says 'opioid' for drugs which have opioid_drug_flag = 'Y', says 'antibiotic' for those drugs which have antibiotic_drug_flag = 'Y', and says 'neither' for all other drugs.
 
